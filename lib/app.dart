5d2f26c58fea2ca01bde6d7f0c5bca69af7e5f23
achievagemilang <achievafuturagemilang@gmail.com>
2024-06-12 20:47:33 +0700

    feat: fcm push notification

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'main/presentation/pages/splash_screen.dart';
@@ -7,12 +9,34 @@ final nav = NavigationService();
 
 void main() => runApp(App());
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  void initState() {
+    super.initState();
+    setupPushNotifications();
+  }
+
+  void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+
+    final token = await fcm.getToken();
+    if (kDebugMode) {
+      print("token $token");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       navigatorKey: nav.navigatorKey,
       routes: nav.routes,
       home: const SplashScreen(),
